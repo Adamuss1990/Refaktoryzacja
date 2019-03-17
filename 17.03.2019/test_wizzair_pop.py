6a5db63b772c2b3f60a6ebdd2858fc416b73642e
student <tester@tester-m>
2019-03-17 14:16:34 +0100

    poprawka

--- 17.03.2019/test_wizzair_pop.py
+++ 17.03.2019/test_wizzair_pop.py
@@ -2,8 +2,11 @@
 import unittest
 from pages.home_page import HomePage
 from pages.login_page import LoginPage
+from pages.registration_page import RegistrationPage
 from selenium import webdriver
 from time import sleep
+import test_data.customers as td
+
 
 class WizzairRegistration(unittest.TestCase):
     def setUp(self):
@@ -24,6 +27,9 @@ class WizzairRegistration(unittest.TestCase):
         home_page.click_zaloguj_button()
         login_page = LoginPage(self.driver)
         login_page.click_rejestracja_button()
+        registration_page= RegistrationPage(self.driver)
+        registration_page.fill_name_field(td.valid_name)
+        registration_page.fill_surname_field(td.valid_surname)
         sleep(4)
 
 if __name__ == '__main__':
